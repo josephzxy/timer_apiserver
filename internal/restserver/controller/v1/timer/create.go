51ee1a0e6f6db50a6dee9c68ca751d73a4be5b8e
josephzxy <josephchuh7@gmail.com>
2021-09-30 01:55:53 +0800

    ✨ feat(rest): use uniformed interface for response writing

--- internal/restserver/controller/v1/timer/create.go
+++ internal/restserver/controller/v1/timer/create.go
@@ -1,29 +1,32 @@
 package timer
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 
 	model "github.com/josephzxy/timer_apiserver/internal/resource/model/v1"
+	resp "github.com/josephzxy/timer_apiserver/internal/restserver/response"
 )
 
 func (tc *TimerController) Create(c *gin.Context) {
 	var timer model.Timer
 	if err := c.ShouldBindJSON(&timer); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": "failed to validate request"})
+		zap.S().Errorw("failed to bind data to model", "err", err)
+		resp.WriteResponse(c, err, nil)
 		return
 	}
 
 	if err := timer.Validate(); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": "failed to validate request"})
+		zap.S().Errorw("data validation failed", "err", err)
+		resp.WriteResponse(c, err, nil)
 		return
 	}
 
 	if err := tc.serviceRouter.Timer().Create(&timer); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"msg": "internal server error"})
+		zap.S().Errorw("failed to create timer", "err", err)
+		resp.WriteResponse(c, err, nil)
 		return
 	}
 
-	c.JSON(http.StatusOK, timer)
+	resp.WriteResponse(c, nil, timer)
 }
