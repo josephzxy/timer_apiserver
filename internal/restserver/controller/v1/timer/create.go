d306a6d3cb98e664020f9f733f882b0c230ad41b
josephzxy <josephchuh7@gmail.com>
2021-09-30 01:19:59 +0800

    ✨ feat(rest): add request validation and business logic for timer create handler

--- internal/restserver/controller/v1/timer/create.go
+++ internal/restserver/controller/v1/timer/create.go
@@ -4,11 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	model "github.com/josephzxy/timer_apiserver/internal/resource/model/v1"
 )
 
 func (tc *TimerController) Create(c *gin.Context) {
-	// request validation
-	// business logic
-	// write response
-	c.JSON(http.StatusOK, gin.H{"hello": "world"})
+	var timer model.Timer
+	if err := c.ShouldBindJSON(&timer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"msg": "failed to validate request"})
+		return
+	}
+
+	if err := timer.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"msg": "failed to validate request"})
+		return
+	}
+
+	if err := tc.serviceRouter.Timer().Create(&timer); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": "internal server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, timer)
 }
