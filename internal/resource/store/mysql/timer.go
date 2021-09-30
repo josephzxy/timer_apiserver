3f92b5fbf6c69fda8d943d157f61ba857ad54e4c
josephzxy <josephchuh7@gmail.com>
2021-09-30 15:36:53 +0800

    ✨ feat(resource): detect app error code from raw gorm error

--- internal/resource/store/mysql/timer.go
+++ internal/resource/store/mysql/timer.go
@@ -4,6 +4,7 @@ import (
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 
+	pkgerr "github.com/josephzxy/timer_apiserver/internal/pkg/err"
 	model "github.com/josephzxy/timer_apiserver/internal/resource/model/v1"
 )
 
@@ -13,9 +14,19 @@ type MySQLTimerStore struct {
 
 func (s *MySQLTimerStore) Create(timer *model.Timer) error {
 	err := s.db.Create(&timer).Error
-	if err != nil {
-		zap.S().Errorw("failed to create timer", "err", err, "data", timer)
-		return err
+	if err == nil {
+		return nil
 	}
-	return nil
+	zap.S().Errorw("failed to create timer", "err", err, "data", timer)
+
+	mysqlErr := GetMySQLErr(err)
+
+	var appErrCode pkgerr.AppErrCode
+	switch mysqlErr {
+	case DUPLICATE_ENTRY:
+		appErrCode = pkgerr.ErrTimerAlreadyExists
+	default:
+		appErrCode = pkgerr.ErrUnknown
+	}
+	return pkgerr.New(appErrCode, err.Error())
 }
