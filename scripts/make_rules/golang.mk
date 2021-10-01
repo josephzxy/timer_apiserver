8c5ad882373a8c9fcfd6c6e540d8071dfc96b34b
josephzxy <josephchuh7@gmail.com>
2021-10-01 14:14:01 +0800

    🐳 chore(makefile): remove pkg internal.resource.v1.model from mock file generating list

--- scripts/make_rules/golang.mk
+++ scripts/make_rules/golang.mk
@@ -73,8 +73,7 @@ go.clean:
 # Supports internal packages only
 # internal.resource.v1.service => github.com/josephzxy/timer_apiserver/internal/resource/v1/service
 MOCK_PKGS := internal.resource.v1.service\
-internal.resource.v1.store\
-internal.resource.v1.model
+internal.resource.v1.store
 
 .PHONY: go.mock
 go.mock: tools.verify.mockgen $(foreach pkg, $(MOCK_PKGS), $(addprefix go.mock., $(pkg)))
