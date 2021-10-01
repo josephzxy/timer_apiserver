e0bcc654320a6694e50168b00ccb7b616419a376
josephzxy <josephchuh7@gmail.com>
2021-10-01 12:01:43 +0800

    ✨ feat(rest): set proper error for failure of model validation and binding

--- internal/restserver/controller/v1/timer/create.go
+++ internal/restserver/controller/v1/timer/create.go
@@ -4,7 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
+	pkgerr "github.com/josephzxy/timer_apiserver/internal/pkg/err"
 	"github.com/josephzxy/timer_apiserver/internal/resource/v1/model"
+
 	resp "github.com/josephzxy/timer_apiserver/internal/restserver/response"
 )
 
@@ -17,13 +19,13 @@ func (tc *TimerController) Create(c *gin.Context) {
 	var timer model.Timer
 	if err := bindJsonFunc(c, &timer); err != nil {
 		zap.S().Errorw("failed to bind data to model", "err", err)
-		resp.WriteResponse(c, err, nil)
+		resp.WriteResponse(c, pkgerr.New(pkgerr.ErrValidation, err.Error()), nil)
 		return
 	}
 
 	if err := validateTimerFunc(&timer); err != nil {
 		zap.S().Errorw("data validation failed", "err", err)
-		resp.WriteResponse(c, err, nil)
+		resp.WriteResponse(c, pkgerr.New(pkgerr.ErrValidation, err.Error()), nil)
 		return
 	}
 
