0c8d91c5dba71207a23a40a878ef0b90044b93c3
josephzxy <josephchuh7@gmail.com>
2021-10-02 17:34:16 +0800

    🧪 test(resource): add test cases for get all timers logic

--- internal/resource/v1/store/mysql/timer/get_test.go
+++ internal/resource/v1/store/mysql/timer/get_test.go
@@ -18,6 +18,13 @@ func monkeyPatch_dbGetByNameFunc(ret error) (restore func()) {
 	return
 }
 
+func monkeyPatch_dbGetAllFunc(ret error) (restore func()) {
+	old := dbGetAllFunc
+	restore = func() { dbGetAllFunc = old }
+	dbGetAllFunc = func(db *gorm.DB, timers *[]model.Timer) error { return ret }
+	return
+}
+
 func Test_TimerStore_GetByName(t *testing.T) {
 	defer monkeyPatch_dbGetByNameFunc(nil)()
 	ts := &TimerStore{&gorm.DB{}}
@@ -43,4 +50,23 @@ func Test_TimerStore_GetByName_otherErr(t *testing.T) {
 	assert.NotNil(t, err)
 	_, ok := err.(*pkgerr.WithCode)
 	assert.False(t, ok)
+}
+
+func Test_TimerStore_GetAll(t *testing.T) {
+	tests := []struct {
+		name  string
+		dbErr error
+	}{
+		{"success", nil},
+		{"failure", errors.New("")},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer monkeyPatch_dbGetAllFunc(tt.dbErr)()
+			ts := &TimerStore{&gorm.DB{}}
+			_, err := ts.GetAll()
+			assert.Equal(t, err, tt.dbErr)
+		})
+	}
 }
