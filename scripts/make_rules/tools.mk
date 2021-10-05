b1f3391e4fbbd7924529f9488562f2ad016a88f1
josephzxy <josephchuh7@gmail.com>
2021-10-05 16:12:52 +0800

    🐳 chore(makefile): add makefile support for generating stub from grpc proto file

--- scripts/make_rules/tools.mk
+++ scripts/make_rules/tools.mk
@@ -21,3 +21,11 @@ tools.install.go-migrate:
 .PHONY: tools.install.mockgen
 tools.install.mockgen:
 	@$(GO) install github.com/golang/mock/mockgen@latest
+
+.PHONY: tools.install.protoc-gen-go
+tools.install.protoc-gen-go:
+	@$(GO) install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+
+.PHONY: tools.install.protoc-gen-go-grpc
+tools.install.protoc-gen-go-grpc:
+	@$(GO) install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
