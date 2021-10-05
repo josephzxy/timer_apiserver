6e761bfb46cd01e03b8df112d18b4fcf17714c94
josephzxy <josephchuh7@gmail.com>
2021-10-05 19:00:59 +0800

    ✨ feat(grpc): return error on one goroutine-run server instance failure

--- internal/grpcserver/server.go
+++ internal/grpcserver/server.go
@@ -1,9 +1,12 @@
 package grpcserver
 
 import (
+	"context"
+	"errors"
 	"net"
 
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
@@ -33,7 +36,7 @@ func (s *GRPCServer) init() {
 	reflection.Register(s.insecureServer)
 }
 
-func (s *GRPCServer) Start() error {
+func (s *GRPCServer) startInsecureServing() error {
 	host := s.cfg.InsecureServing.Addr()
 	lis, err := net.Listen("tcp", host)
 	if err != nil {
@@ -41,10 +44,28 @@ func (s *GRPCServer) Start() error {
 	}
 
 	zap.S().Infow("grpc server insecure serving is serving", "host", host)
-	if err := s.insecureServer.Serve(lis); err != nil {
-		return err
-	}
-	return nil
+	return s.insecureServer.Serve(lis)
+}
+
+func (s *GRPCServer) Start() error {
+	eg, ctx := errgroup.WithContext(context.Background())
+	eg.Go(func() error {
+		failed := make(chan struct{}, 1)
+		go func() {
+			if err := s.startInsecureServing(); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
+				zap.S().Errorf("error occured during grpc server insecure serving", "err", err)
+				failed <- struct{}{}
+			}
+		}()
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-failed:
+			return errors.New("grpc server insecure serving failed")
+		}
+	})
+	return eg.Wait()
 }
 
 func (s *GRPCServer) Stop() {
