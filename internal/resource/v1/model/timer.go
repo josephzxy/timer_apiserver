f424afdb303d08ddd023021824eda819b20f4067
josephzxy <josephchuh7@gmail.com>
2021-10-07 21:45:53 +0800

    ✨ feat(resource): add not blank validation for TimerCore.Name & TimerCore.TriggerAt

--- internal/resource/v1/model/timer.go
+++ internal/resource/v1/model/timer.go
@@ -4,18 +4,26 @@ import (
 	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator/v10/non-standard/validators"
 	"gorm.io/gorm"
 )
 
+var val *validator.Validate
+
+func init() {
+	val = validator.New()
+	val.RegisterValidation("notblank", validators.NotBlank)
+}
+
 // TimerCore contains fields that can be specified directly via APIs
 type TimerCore struct {
-	Name      string    `json:"name" gorm:"index:uniq_name_alive,unique,priority:1" validate:"required"`
-	TriggerAt time.Time `json:"triggerAt" gorm:"index:idx_trigger_at" validate:"required,gte=time.Now().Add(time.Minute)"`
+	Name      string    `json:"name" gorm:"index:uniq_name_alive,unique,priority:1" validate:"required,notblank"`
+	TriggerAt time.Time `json:"triggerAt" gorm:"index:idx_trigger_at" validate:"required,gte=time.Now().Add(time.Minute),notblank"`
 }
 
 // ValidateTimerCore validates the given value of TimerCore
 func ValidateTimerCore(tc *TimerCore) error {
-	return validator.New().Struct(tc)
+	return val.Struct(tc)
 }
 
 // Timer is the data model for RESTful resource timer.
@@ -33,5 +41,5 @@ func (t *Timer) TableName() string {
 
 // ValidateTimer validates the given value of Timer
 func ValidateTimer(t *Timer) error {
-	return validator.New().Struct(t)
+	return val.Struct(t)
 }
